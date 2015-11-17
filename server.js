f4bc954d706cedc5186cea52638704626cd925cc
Brian Nguyen <brianacnguyen@gmail.com>
2015-11-17 11:13:40 -0800

    deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 mongoose.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
 
 // configure our server with all the middleware and and routing
-require('./config/middleware.js')(app, express);
+require('./server/config/middleware.js')(app, express);
 
 var port;
 if (process.env.PORT) {
