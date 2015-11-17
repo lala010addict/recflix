ee9578dd345ff58d857c0d7bd3c9bb828152dac6
JieYing Peng <jipeng@syr.edu>
2015-11-17 10:28:19 -0800

    DB and auth

---
+++ server/config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '848126855303146', // your App ID
+        'clientSecret'  : 'cff690500dd1416eb1f86bce0d066769', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '60150280473-98aa76m7qm2ne6rri3rvhghgf18ofh0o.apps.googleusercontent.com',
+        'clientSecret'  : 'Uz5rdXOBOkeGmNYYU1x_n26_',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
