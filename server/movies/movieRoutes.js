dab59f6a22a1678d671d443087f4b683f187473a
Brian Nguyen <brianacnguyen@gmail.com>
2015-11-18 17:25:42 -0800

    added pseudocodes to get recommendations, need to fix rest of movie controller, rerouted movieroutes

--- server/movies/movieRoutes.js
+++ server/movies/movieRoutes.js
@@ -3,16 +3,14 @@ var movieController = require('./movieController.js');
 module.exports = function (app) {
   // app === linkRouter injected from middleware.js
 
-  // app.param will hijack any request with a 'code' parameter on in
-  // like line 16 below. That code will actually be the shortned url
-  // so the real URL will be pre fetched from mongo and attached to
-  // req.navLink before it reaches line 16.
-  app.get('/savedMovies', movieController.savedMovies)
+  // any get request to /api/movies/getRecommendations will be controlled
+  // by the getRecommendations function
   app.get('/getRecommendations', movieController.getRecommendations);
-  app.post('/newMovie', movieController.newMovie);
 
+  // any get/post/delete request to /api/movies/will be controlled
+  // by the movieController
   app.route('/')
     .get(movieController.savedMovies)
     .post(movieController.newMovie)
-    // .delete(movieController.deleteMovie);
+    .delete(movieController.deleteMovie);
 };
