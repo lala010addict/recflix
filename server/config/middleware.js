fd33ffc0450e90868c6cd59eea8433e3953c996c
Brian Nguyen <brianacnguyen@gmail.com>
2015-11-18 18:30:52 -0800

    tried fixing middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -18,7 +18,7 @@ module.exports = function(app, express) {
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
   app.use(cookieParser());
-  //register /api/... and assign routers that will take care of it
+  //register /api/... and assign routers that will take care of it 
   app.use('/api/movies', movieRouter);
   app.use('/api/users', userRouter); 
 
