a3cecbfea68490ea3e8aff51127c4b4416e629f9
JieYing Peng <jipeng@syr.edu>
2015-11-18 16:28:03 -0800

    merge

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,10 +6,8 @@ var morgan = require('morgan'), // used for logging incoming request
 
 module.exports = function(app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
-  // var userRouter = express.Router();
-  // var linkRouter = express.Router();
-  // app.set('view engine', 'ejs');
-  // app.set('views', __dirname + '/views');
+  var userRouter = express.Router();
+  var movieRouter = express.Router();
 
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({
@@ -21,15 +19,13 @@ module.exports = function(app, express) {
   app.use(express.static(__dirname + '/../../client'));
   app.use(cookieParser());
 
-  // app.use('/api/users', userRouter); // use user router for all user request
+  app.use('/api/users', userRouter); // use user router for all user request
 
-  // authentication middleware used to decode token and made available on the request
-  //app.use('/api/links', helpers.decode);
-  // app.use('/api/links', linkRouter); // user link router for link request
+  app.use('/api/movies', movieRouter);
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
   // inject our routers into their respective route files
-  // require('../users/userRoutes.js')(userRouter);
-  // require('../links/linkRoutes.js')(linkRouter);
+  require('../users/userRoutes.js')(userRouter);
+  require('../movies/movieRoutes.js')(movieRouter);
 };
