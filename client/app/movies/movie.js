a177a90854601d71c327d8b7c5704eecdcff0d7d
Omar Sial <omar.sial1@gmail.com>
2015-11-19 18:47:09 -0500

    api testing

--- client/app/movies/movie.js
+++ client/app/movies/movie.js
@@ -4,12 +4,23 @@ angular.module('movieApp.movies', [])
   // Your code here
   // add this movie to users playlist
   // modal for more info
-  $scope.films = Movies.tasteKid; //list of films returned
+  $scope.films = Movies.movies; //list of films returned
   $scope.watchList = Movies.watchList;
   $scope.logger3 = function() {
     console.log('hello from MovieController');
+    console.log(Movies.movies);
+
     console.log($scope.films);
   };
+  $scope.handleSearch = function() {
+      console.log('$scope.movies' , $scope.movies);
+      Movies.handleSearch($scope.searchString)
+        .then(function (movies) {
+          $scope.films = movies;
+        });
+      // $scope.movies = newMovies;
+      console.log('$scope.movies' , $scope.movies);
+    };
 })
 .directive('movieCard', function(){
   return {
