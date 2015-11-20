0a9da1741533885c3b9aba83a38de0a462d8c4bc
Omar Sial <omar.sial1@gmail.com>
2015-11-19 19:27:27 -0500

    latest

--- client/app/movies/movie.js
+++ client/app/movies/movie.js
@@ -5,32 +5,23 @@ angular.module('movieApp.movies', [])
   // add this movie to users playlist
   // modal for more info
   $scope.films = Movies.movies; //list of films returned
+  $scope.searchString = '';
+  console.log($scope.films);
   $scope.watchList = Movies.watchList;
   $scope.logger3 = function() {
     console.log('hello from MovieController');
     console.log(Movies.movies);
-
     console.log($scope.films);
   };
-  $scope.handleSearch = function() {
-      console.log('$scope.movies' , $scope.movies);
-      Movies.handleSearch($scope.searchString)
+  $scope.handleSearch = function(str) {
+      Movies.handleSearch(str)
         .then(function (movies) {
-          $scope.films = movies;
+          Movies.movies = movies;
+          $scope.films = Movies.movies;
+          console.log('Movie.movies' , Movies.movies);
+          console.log('$scope.films' , $scope.films);
         });
       // $scope.movies = newMovies;
-      console.log('$scope.movies' , $scope.movies);
     };
 })
-.directive('movieCard', function(){
-  return {
-    restrict: 'E',
-    scope: {
-      data: '='
-    },
-    templateUrl: 'app/movies/movie.html',
-    controller: function($scope) {
-      console.log($scope.watchList);
-    }
-  };
-});
+
--- client/app/search/search.js
+++ client/app/search/search.js
@@ -6,28 +6,30 @@ angular.module('movieApp.search', [])
     $scope.logger = function() {
       console.log($scope.searchString);
     };
-  $scope.handleSearch = function() {
-      console.log('$scope.movies' , $scope.movies);
-      Movies.handleSearch($scope.searchString)
-        .then(function (movies) {
-          $scope.movies = movies;
-        });
-      // $scope.movies = newMovies;
-      console.log('$scope.movies' , $scope.movies);
-    };
+  // $scope.handleSearch = function() {
+  //     console.log('$scope.movies' , $scope.movies);
+  //     Movies.handleSearch($scope.searchString)
+  //       .then(function (movies) {
+  //         Movies.movies = movies;
+  //       });
+  //     // $scope.movies = newMovies;
+  //     console.log('$scope.movies' , $scope.movies);
+  //   };
     
   }) // serach bar --> search ---> take search query and do a GET to our server --> return the data 
       // to the 'movies' array 
   .factory('Movies', function (Search) {
     var movies = [];
+    var searchString = '';
     var watchList = [];
     var handleSearch = function(str) {
       return Search.getMovies(str)
         .then(function (searchResults) {
           this.movies = searchResults;
-          console.log("from handle search");
-          console.log(searchResults, 'this is the search results');
-          console.log(this.movies, 'this is this.movies')
+          // console.log("from handle search");
+          // console.log(searchResults, 'this is the search results');
+          // console.log(this.movies, 'this is this.movies');
+          return this.movies;
         })
         .catch(function() {
           console.log("we have an error in handleSearch");
@@ -37,6 +39,7 @@ angular.module('movieApp.search', [])
     return {
       movies: movies,
       watchList: watchList,
-      handleSearch: handleSearch
+      handleSearch: handleSearch,
+      searchString: searchString
     };
   });
