9b4f51b8ae7b5d25731c59335233fbd251ed5c0c
Omar Sial <omar.sial1@gmail.com>
2015-11-19 20:57:17 -0500

    deploy

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -6,16 +6,5 @@ angular.module('movieApp.search', [])
     $scope.logger = function() {
       console.log($scope.searchString);
     };
-  // $scope.handleSearch = function() {
-  //     console.log('$scope.movies' , $scope.movies);
-  //     Movies.handleSearch($scope.searchString)
-  //       .then(function (movies) {
-  //         Movies.movies = movies;
-  //       });
-  //     // $scope.movies = newMovies;
-  //     console.log('$scope.movies' , $scope.movies);
-  //   };
-    
-  }) ;// serach bar --> search ---> take search query and do a GET to our server --> return the data 
-      // to the 'movies' array 
+  }); 
 
